5a673b9f6b9ed974445732a14458a8120b4f272e
urick0s <bluiote@gmail.com>
2021-11-19 06:06:15 +0300

    sleep break2

---
+++ tests/test.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	"errors"
+	"testing"
+
+	"testMail"
+)
+
+func TestClient(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "a4bc2d5e", expected: "aaaabccddddde"},
+		{input: "abccd", expected: "abccd"},
+		{input: "", expected: ""},
+		{input: "aaa0b", expected: "aab"},
+		// uncomment if task with asterisk completed
+		// {input: `qwe\4\5`, expected: `qwe45`},
+		// {input: `qwe\45`, expected: `qwe44444`},
+		// {input: `qwe\\5`, expected: `qwe\\\\\`},
+		// {input: `qwe\\\3`, expected: `qwe\3`},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := Unpack(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
